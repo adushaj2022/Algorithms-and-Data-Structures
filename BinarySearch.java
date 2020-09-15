a1f356578c6d05665f7d7291268537aa575b6eb4
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2020-09-14 21:24:57 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -5,8 +5,9 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		int target = 7;
 		int arr[] = {2, 3, 4, 5, 7, 8, 9, 11, 13, 19, 22};
-		System.out.print(target + " was found at position " + findIndexPosition(arr, target));
+		System.out.println(target + " was found at position " + searchBin(arr, 0, arr.length -1, target));
 		
+		System.out.println(target + " was found at position " + findIndexPosition(arr, target));
 	}
 
 	private static int findIndexPosition(int[] arr, int target) {
@@ -30,5 +31,19 @@ public class BinarySearch {
 		}
 		
 		return -1;
+	}
+	
+	//recursively
+	
+	private static int searchBin(int[]arr, int left, int right, int target){
+		
+		int midpoint = (left + right) / 2;
+		
+		if(left > right){return -1;}
+		
+		if (arr[midpoint] == target){return midpoint;}
+		
+		return (arr[midpoint] > target) ? (searchBin(arr, left, midpoint - 1, target)) : (searchBin(arr, midpoint + 1, right, target));
+		
 	}
 }

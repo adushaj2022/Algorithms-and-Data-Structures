71a7150933707913830711a5961ca73c81217190
Anthony Dushaj <47046091+adushaj2022@users.noreply.github.com>
2020-11-13 20:16:51 -0500

    Create InOrder.java

---
+++ InOrder.java
@@ -0,0 +1,14 @@
+class Solution {
+    List<Integer> l = new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null){
+            return l;
+        }
+        
+        inorderTraversal(root.left);
+        l.add(root.val);
+        inorderTraversal(root.right);
+        
+        return l;
+    }
+}
